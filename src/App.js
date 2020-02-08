51aad4d354538ed470fcdae91d2168a219e77344
Lex James <Phlex06@MacBook-Pro-3.local>
2020-02-08 22:48:25 +0000

    Move background component into own file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import MobileMenuButton from './frontend/components/MobileMenuButton/MobileMenuButton'
 import MobileMenu from './frontend/components/MobileMenu/MobileMenu'
+import Background from './frontend/components/Background/Background'
 import './App.css';
 
 function App() {
@@ -25,19 +26,6 @@ function App() {
         toggleMenuVisible={() => toggleMenuVisible()}
         darkMode={darkMode}
       />
-    </div>
-  );
-}
-
-function Background() {
-  return (
-    <div>
-      <div className='background'>
-      </div>
-      <div className='title'>
-        <h1 id='nickname'>LJ</h1>
-        <h1 id='job'>Junior Developer</h1>
-      </div>
     </div>
   );
 }
---
+++ src/frontend/components/Background/Background.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Background.css'
+
+export default function Background() {
+    return (
+        <div>
+            <div className='background'>
+            </div>
+            <div className='title'>
+                <h1 id='nickname'>LJ</h1>
+                <h1 id='job'>Junior Developer</h1>
+            </div>
+        </div>
+    );
+}
